e77d7e6d1abce7d2bf090813bfe766401eb2abc0
MaiNhanIMIC <mainhan.imic@gmail.com>
2025-07-20 09:17:53 +0700

    file

--- 225024/main.c
+++ 225024/main.c
@@ -2,25 +2,71 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "link_list.h"
-#include "RingBuffer.h"
-RingBuffer_t ring_bf;
-int ring_bf_item;
+typedef enum
+{
+    MALE,
+    FEMALE
+} gender_t;
+typedef struct
+{
+    char name[32];
+    int age;
+    gender_t gender;
+    float literature;
+    float math;
+    float avg;
+} student_t;
+
 int main() {
-    
-    CreateRingBuffer(&ring_bf, 3);
+    FILE* fp = fopen("D:/Temp_Dir/DanhSachHocSinh.csv", "r");
+    if (fp == NULL)
+    {
+        printf("failed to open file \n");
+        return -1;
+    }
+    char c = 0;
+    char data_buf[1024] = { 0 };
+    int index = 0;
+    do
+    {
+        c = fgetc(fp);
+        data_buf[index++] = c;
+    } while (c != EOF);
+    fclose(fp);
+    int num_student = 0;
+    int len_data = strlen(data_buf);
+    for (int i = 0; i < len_data; i++)
+    {
+        if (data_buf[i] == '\n')
+            num_student++;
+    }
 
-    PutItemToRingBuffer(&ring_bf, 1);
-    PutItemToRingBuffer(&ring_bf, 2);
-    PutItemToRingBuffer(&ring_bf, 3);
-    PutItemToRingBuffer(&ring_bf, 4);
-    GetItemFromRingBuffer(&ring_bf, &ring_bf_item);
-    GetItemFromRingBuffer(&ring_bf, &ring_bf_item);
-    GetItemFromRingBuffer(&ring_bf, &ring_bf_item);
-    PutItemToRingBuffer(&ring_bf, 4);
-    GetItemFromRingBuffer(&ring_bf, &ring_bf_item);
+    student_t* student_list = (student_t *)calloc(num_student, sizeof(student_t));
+    if (student_list == NULL) return -1;
 
-    DeleteRingBuffer(&ring_bf);
+    char* data = data_buf;
+
+    for (int i = 0; i < num_student; i++)
+    {
+        char line_data[256] = { 0 };
+        int index = 0;
+        do
+        {
+            line_data[index++] = *data;
+        } while (*data++ != '\n');
+        char gender_buf[8] = { 0 };
+        sscanf(line_data, "%[^,],%d,%[^,],%f,%f", student_list[i].name, &student_list[i].age, gender_buf, &student_list[i].literature, &student_list[i].math);
+        if (strcmp(gender_buf, "NAM") == 0)
+            student_list[i].gender = MALE;
+        else if (strcmp(gender_buf, "NU") == 0)
+            student_list[i].gender = FEMALE;
+        student_list[i].avg = (student_list[i].literature + student_list[i].math) / 2;
+        printf("");
+    }
+
+
 
     return 0;
 }
+
+//%s scan string, đến khi nào hặp ký tự space hoặc \n thì no sẽ ngừng lại
