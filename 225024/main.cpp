0d682a68d320c5be68af9b6fe186172aae27c25a
MaiNhanIMIC <mainhan.imic@gmail.com>
2025-08-17 13:38:55 +0700

    mang cpp

---
+++ 225024/main.cpp
@@ -0,0 +1,41 @@
+﻿#include <stdio.h>
+#include <malloc.h>
+#include <iostream>
+#include "phan_so.h"
+using namespace std;
+struct phan_so
+{
+	int tu;
+	int mau;
+};
+
+template <class kieu_du_lieu>
+class mang
+{
+public:
+	int so_luong;
+	kieu_du_lieu* dia_chi;
+	mang(int sl)
+	{
+		so_luong = sl;
+		dia_chi = (kieu_du_lieu*)calloc(so_luong, sizeof(kieu_du_lieu));
+	}
+
+	kieu_du_lieu& operator[](int i)
+	{
+		return dia_chi[i];
+	}
+
+	~mang()
+	{
+		free(dia_chi);
+	}
+};
+
+int main()
+{
+
+
+
+	return 0;
+}
